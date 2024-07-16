a5770547d1b8d3213e14fe8ca3b531297034f61f
Ashish Chaubey <ashishchaubey866@gmail.com>
2024-07-16 22:11:48 +0530

    feat: change index.css route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-7xl font-bold underline ">Hello world!</h1>;
+  return <h1 className="text-9xl font-bold">Hello world!</h1>;
 }
 
 export default App;
