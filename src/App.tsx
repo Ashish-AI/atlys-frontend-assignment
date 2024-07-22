4e8adef81d5695a41a551f35978062b8a0bf7b18
Ashish Chaubey <ashishchaubey866@gmail.com>
2024-07-22 22:33:07 +0530

    fix: rendering of screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,26 @@ import React from "react";
 import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className=" bg-dark flex items-center justify-center min-h-screen ">
-      <Router>
+    <Router>
+      <div className="bg-dark flex items-center justify-center min-h-screen">
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
-      </Router>
-      <Home />
-    </div>
+      </div>
+    </Router>
   );
 }
 
